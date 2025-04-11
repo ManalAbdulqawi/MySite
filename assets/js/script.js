1847bcb84994acc88c203181dd1735a018278740
ManalAbdulqawi <denever_2008@windowslive.com>
2025-04-11 01:02:38 +0100

    add popup thank you message after submit contact me form

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,25 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+
+function openPopup(){
+
+  let name=document.getElementById("name");
+  let email=document.getElementById("email");
+  let phone=document.getElementById("phone");
+  let subject=document.getElementById("subject");
+  let message=document.getElementById("Message");
+  const form=document.querySelector("form");
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+  })
+  if(name.value !== "" && email.value !=="" && phone.value !=="" && subject.value !== "" && message.value !=="" ){
+  popup.classList.add("open-popup");
+}
+
+}
+
+function closePopup(){
+  popup.classList.remove("open-popup")
+}
